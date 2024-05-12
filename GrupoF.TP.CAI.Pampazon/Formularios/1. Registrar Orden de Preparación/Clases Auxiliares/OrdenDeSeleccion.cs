a4d319caca724c8409a751dafd4400ad81946bec
Fersanna <flsanna@hotmail.com>
2024-05-12 17:44:01 -0300

    Join Group

---
+++ GrupoF.TP.CAI.Pampazon/Formularios/1. Registrar Orden de Preparación/Clases Auxiliares/OrdenDeSeleccion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoF.TP.CAI.Pampazon.Clases_Auxiliares
+{
+    public class OrdenDeSeleccion
+    {
+        private static int contadorOrdenes = 0;
+        public string NumeroDeOrden {get; set; }
+
+        public DateTime Fecha {get; set; }
+
+        public string CodigoCliente {get; set; }    
+        public string CodigoTransportista {get; set;}
+        public string EstadoOrden{get; set; }
+
+        public List<Productos> ProductosOrden {get; set; }
+
+
+        public OrdenDeSeleccion()
+        {
+            ProductosOrden = new List<Productos>();
+
+            NumeroDeOrden = contadorOrdenes.ToString();
+            contadorOrdenes++;
+        }
+
+
+    }
+}
---
+++ GrupoF.TP.CAI.Pampazon/Formularios/1. Registrar Orden de Preparación/Clases Auxiliares/Productos.cs
@@ -0,0 +1,14 @@
+﻿namespace GrupoF.TP.CAI.Pampazon.Clases_Auxiliares
+{
+    public class Productos
+    {
+        public string IdProducto { get; set; }
+        public string Descripcion { get; set; }
+        public  string Posicion {get ; set;}
+        public int Cantidad {get; set;}
+
+        public Productos()
+        {
+        }
+    }
+}
