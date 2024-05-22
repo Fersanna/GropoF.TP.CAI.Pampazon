eb9b02aaea280ba0852efaa2bcc2d0b786cf3787
Fersanna <flsanna@hotmail.com>
2024-05-22 20:25:55 -0300

    Comienzo caso de uso 4

--- GrupoF.TP.CAI.Pampazon/Formularios/4. Confirmar Orden Seleccionada/ConfirmarOrdenSeleccionadaForm.cs
+++ GrupoF.TP.CAI.Pampazon/Formularios/4. Confirmar Orden Seleccionada/ConfirmarOrdenSeleccionadaForm.cs
@@ -1,10 +1,12 @@
-﻿using GrupoF.TP.CAI.Pampazon.Formularios._3._Buscar_Posición;
+﻿using GrupoF.TP.CAI.Pampazon.Clases_Auxiliares;
+using GrupoF.TP.CAI.Pampazon.Formularios._3._Buscar_Posición;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,7 +15,8 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 namespace GrupoF.TP.CAI.Pampazon.Formularios._4._Confirmar_Orden_Seleccionada
 {
     public partial class ConfirmarOrdenSeleccionadaForm : Form
-    {
+    {   
+        ConfirmarOrdenSeleccionadaFormModel model;
         public ConfirmarOrdenSeleccionadaForm()
         {
             InitializeComponent();
@@ -21,7 +24,22 @@ namespace GrupoF.TP.CAI.Pampazon.Formularios._4._Confirmar_Orden_Seleccionada
 
         private void ConfirmarOrdenSeleccionadaForm_Load(object sender, EventArgs e)
         {
-            AgregarDatosDePrueba();
+             model = new ConfirmarOrdenSeleccionadaFormModel();
+
+              foreach (OrdenDePreparacion ordenesSeleccionada in model.OrdenEnSeleccion)
+            {
+                if (ordenesSeleccionada.EstadoOrden == "En selección")
+                {
+                    ListViewItem item = new ListViewItem(ordenesSeleccionada.NumeroDeOrden);
+                    item.SubItems.Add(ordenesSeleccionada.CodigoCliente);
+                    item.SubItems.Add(ordenesSeleccionada.Fecha.ToString());
+                    item.SubItems.Add(ordenesSeleccionada.CodigoTransportista);
+                    item.SubItems.Add(ordenesSeleccionada.EstadoOrden);
+
+                    listOrdenesEnSeleccion.Items.Add(item);
+                }
+            }
+            
         }
 
         private void CancelarBtn_Click(object sender, EventArgs e)
@@ -46,23 +64,6 @@ namespace GrupoF.TP.CAI.Pampazon.Formularios._4._Confirmar_Orden_Seleccionada
             }
         }
 
-        private void AgregarDatosDePrueba()
-        {
-            ListViewItem item1 = new ListViewItem("P-000001");
-            item1.SubItems.Add("001");
-            item1.SubItems.Add("13/05/24");
-            item1.SubItems.Add("Transportista 1");
-            item1.SubItems.Add("En selección");
-
-            ListViewItem item2 = new ListViewItem("P-000002");
-            item2.SubItems.Add("002");
-            item2.SubItems.Add("13/05/24");
-            item2.SubItems.Add("Transportista 2");
-            item2.SubItems.Add("En selección");
-
-            // Agregar elementos al ListView
-            listOrdenesEnSeleccion.Items.Add(item1);
-            listOrdenesEnSeleccion.Items.Add(item2);
-        }
+      
     }
 }
---
+++ GrupoF.TP.CAI.Pampazon/Formularios/4. Confirmar Orden Seleccionada/ConfirmarOrdenSeleccionadaFormModel.cs
@@ -0,0 +1,48 @@
+﻿using GrupoF.TP.CAI.Pampazon.Clases_Auxiliares;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoF.TP.CAI.Pampazon.Formularios._4._Confirmar_Orden_Seleccionada
+{
+    internal class ConfirmarOrdenSeleccionadaFormModel
+    {
+         public List<OrdenDePreparacion> OrdenEnSeleccion { get; set; } = new List<OrdenDePreparacion>
+        {
+            new OrdenDePreparacion
+            {
+                NumeroDeOrden = "P-000001",
+                CodigoCliente = "Moron Construcciones",
+                Fecha = new DateTime(2024, 5, 13),
+                CodigoTransportista = "Transportista 1",
+                EstadoOrden = "En selección"
+            },
+            new OrdenDePreparacion
+            {
+                NumeroDeOrden = "P-000002",
+                CodigoCliente = "Fortin SRL",
+                Fecha = new DateTime(2024, 5, 13),
+                CodigoTransportista = "Transportista 2",
+                EstadoOrden = "En selección"
+            },
+            new OrdenDePreparacion
+            {
+                NumeroDeOrden = "P-000003",
+                CodigoCliente = "Valentin Gomez S.A",
+                Fecha = new DateTime(2024, 5, 14),
+                CodigoTransportista = "Transportista 3",
+                EstadoOrden = "En selección"
+            },
+            new OrdenDePreparacion
+            {
+                NumeroDeOrden = "P-000004",
+                CodigoCliente = "Almada S.A",
+                Fecha = new DateTime(2024, 5, 15),
+                CodigoTransportista = "Transportista 4",
+                EstadoOrden = "En selección"
+            }
+        };
+    }
+}
